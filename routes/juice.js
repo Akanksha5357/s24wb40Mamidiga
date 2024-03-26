40d677c1d7056d6f9534aa2f7bb0347f272215ea
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-03-26 18:57:26 -0500

    changes made

--- routes/juice.js
+++ routes/juice.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('juice', { title: 'Search Results for fruit' });
+  res.render('juice', { title: 'Search Results for Juice' });
 });
 
 module.exports = router;
