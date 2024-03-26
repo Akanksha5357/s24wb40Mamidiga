4f0111c08866147251e35ebc65fc1cfaf8865ae3
Akanksha5357 <156253285+Akanksha5357@users.noreply.github.com>
2024-03-26 18:51:51 -0500

    added juice endpoint

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/juice', dressRouter); //juice endpoint added
-a
+app.use('/juice', juiceRouter); //juice endpoint added
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
